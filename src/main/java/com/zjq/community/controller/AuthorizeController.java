0a17c4ddbc5a94ac4fe46fc3af48a698a2eae566
ZjqBlack <446161136@qq.com>
2019-12-12 22:52:43 +0800

    把github变量配置到properties

--- src/main/java/com/zjq/community/controller/AuthorizeController.java
+++ src/main/java/com/zjq/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.zjq.community.adapter.GithubProvider;
 import com.zjq.community.dto.AccessTokenDto;
 import com.zjq.community.dto.GithubUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,13 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
 //    用户点击登录之后，授权给git，git会返回我们指定的页面，并携带code信息
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
@@ -22,9 +30,9 @@ public class AuthorizeController {
 
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setCode(code);
-        accessTokenDto.setRedirect_uri("http://localhost:8887/callback");
-        accessTokenDto.setClient_id("f2f363d8baeb8b251fc7");
-        accessTokenDto.setClient_secret("3649bf20e17af82400f337ce6425d506823e4a27");
+        accessTokenDto.setRedirect_uri(redirectUri);
+        accessTokenDto.setClient_id(clientId);
+        accessTokenDto.setClient_secret(clientSecret);
         accessTokenDto.setState(state);
 
         String accessToken = githubProvider.getAccessToken(accessTokenDto);
