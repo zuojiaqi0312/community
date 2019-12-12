5874d80e76c64c1adc40b955e3c8d7f2b13d2218
ZjqBlack <446161136@qq.com>
2019-12-12 18:55:59 +0800

    init reop

---
+++ src/test/java/com/zjq/community/communtity/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjq.community.communtity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
